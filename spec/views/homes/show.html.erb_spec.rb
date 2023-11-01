0be5d6c66ee693cb6150258396e56651382dd99c
Lukáš Pokorný <luk4s.pokorny@gmail.com>
2023-11-01 21:05:32 +0100

    ireset button

---
+++ spec/views/homes/show.html.erb_spec.rb
@@ -0,0 +1,22 @@
+RSpec.describe "homes/show", logged: true do
+
+  helper_method :current_user
+
+  before do
+    allow(view).to receive(:current_user).and_return(Symphonia::User.current)
+  end
+
+  context "with home" do
+    let(:home) { create :my_home, user: Symphonia::User.current }
+
+    before do
+      assign(:home, home)
+      allow(home).to receive(:duplex).and_return spy
+    end
+
+    it "renders without error" do
+      render
+      expect(rendered).to include t :legend_atrea
+    end
+  end
+end
