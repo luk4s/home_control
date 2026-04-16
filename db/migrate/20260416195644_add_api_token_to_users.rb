0ebe16b4441f5a635f856f48280d9ede8c54a8a3
Lukáš Pokorný <luk4s.pokorny@gmail.com>
2026-04-16 22:45:05 +0200

    refactor: migration for prevent model touch

--- db/migrate/20260416195644_add_api_token_to_users.rb
+++ db/migrate/20260416195644_add_api_token_to_users.rb
@@ -1,17 +1,28 @@
 class AddApiTokenToUsers < ActiveRecord::Migration[8.0]
 
   def change
+    # Add column as nullable first
     add_column :users, :api_token, :string
-    add_index :users, :api_token, unique: true
 
-    # Backfill existing users with secure tokens
+    # Backfill existing users with secure random tokens using PostgreSQL
     reversible do |dir|
       dir.up do
-        User.find_each do |user|
-          user.update_column(:api_token, SecureRandom.base58(24))
-        end
+        # Use gen_random_uuid() to generate unique tokens for existing users
+        # This requires pgcrypto extension (already enabled in this app)
+        execute <<-SQL
+          UPDATE users#{' '}
+          SET api_token = encode(gen_random_bytes(18), 'base64')
+          WHERE api_token IS NULL;
+        SQL
+      end
+    end
 
-        # Now make it non-nullable
+    # Add unique index
+    add_index :users, :api_token, unique: true
+
+    # Make column non-nullable after backfill
+    reversible do |dir|
+      dir.up do
         change_column_null :users, :api_token, false
       end
     end
